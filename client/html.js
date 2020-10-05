13b7b6d5d044dfeaec0aecbd621f09a01f1fef6a
anvar2404 <anvarabdullaev14@gmail.com>
2020-10-05 20:33:00 +0600

    font

--- client/html.js
+++ client/html.js
@@ -6,6 +6,11 @@ const Html = ({ body }) => {
       <meta charset="UTF-8">
       <link rel="stylesheet" type="text/css" href="/css/main.css" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="stylesheet" type="text/css" href="/css/main.css" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500;700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+        <title>SHOP</title>
     </head>
     <body>
       <div id="root">${body}</div>
